8814a4f93ebc327b13991cb67809f975a7c7b1cf
Naveen_M <naveenmauryagng@gmail.com>
2026-09-15 00:35:51 +0530

    Fix Prisma Docker build configuration

--- Server/prisma.config.js
+++ Server/prisma.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
     },
     engine: "classic",
     datasource: {
-        // url: env("DATABASE_URL"),
-        url: process.env.DATABASE_URL ?? "",
+        url: env("DATABASE_URL"),        
     },
 });
 //# sourceMappingURL=prisma.config.js.map
